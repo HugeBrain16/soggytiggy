67e7235013b39e3e1cfa50ae9bcfee3a1e9a394d
HugeBrain16 <hugebrain16@gmail.com>
2024-01-28 08:20:35 +0700

    make use of imgur thing #2

--- modal.js
+++ modal.js
@@ -17,16 +17,34 @@ function openModal(img) {
 }
 
 document.body.appendChild(modal);
-modalClose.onclick = function() {
+modalClose.onclick = function () {
   modal.style.display = "none";
+};
+
+function getImage(url) {
+  var result = "";
+  var string = url.split("").reverse().join("");
+  var skipmode = false;
+
+  for (let c of string) {
+    if (skipmode) {
+      skipmode = false;
+      continue;
+    }
+
+    if (c === ".") skipmode = true;
+    result += c;
+  }
+
+  return result.split("").reverse().join("");
 }
 
-window.onload = function() {
+window.onload = function () {
   var thumbs = document.getElementsByClassName("gallery-thumbnail");
-  
+
   for (let i = 0; i < thumbs.length; i++) {
-    thumbs[i].onclick = function() {
-      openModal(thumbs[i].style.backgroundImage.split(/"/)[1]);
-    }
+    thumbs[i].onclick = function () {
+      openModal(getImage(thumbs[i].style.backgroundImage.split(/"/)[1]));
+    };
   }
-}
+};
